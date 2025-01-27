ce51420ae0b9d8bad41b846e51231f4f1a779edb
karacaalp <karacaalp@users.noreply.github.com>
2025-01-27 02:29:22 +0000

    deploy: 5d312a0ca18f8550bd8fe292bddc5234db7a9d05

---
+++ assets/dateFormats-3GsDklRu.js
@@ -0,0 +1,1 @@
+const i={DATE_OPTIONS:{day:"2-digit",month:"2-digit",year:"numeric"},TIME_OPTIONS:{timeZone:"UTC",hour:"2-digit",minute:"2-digit"},LOCALE:{DATE:"en-US",TIME:"en-US"}};export{i as D};
